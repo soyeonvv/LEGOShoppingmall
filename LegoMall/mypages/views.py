93c36c3064b4941f1c810aa73f66fb822360cb87
soyeonvv <thdusdlvv@naver.com>
2021-12-18 20:35:47 +0900

    홈페이지 만들기

--- LegoMall/mypages/views.py
+++ LegoMall/mypages/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from shopping.models import Post
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'mypages/homepage.html')
+    recent_posts = Post.objects.order_by('-pk')[:3]
+    return render(request, 'mypages/homepage.html',
+                {'recent_posts' : recent_posts})
 
 def mypage(request) :
     return render(request, 'mypages/mypage.html')
