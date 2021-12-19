d443e3982f1229b288b43ae1fa2b09282e214f17
soyeonvv <thdusdlvv@naver.com>
2021-12-19 22:54:38 +0900

    django allauth template 수정

--- LegoMall/LegoMall/settings.py
+++ LegoMall/LegoMall/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'LegoMall.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
